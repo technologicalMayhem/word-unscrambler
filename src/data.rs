f86db3ee4afdab2fcf265f6dc92d15cc494fc630
technologicalMayhem <wolfshund98@gmail.com>
2023-02-23 20:14:54 +0000

    Implement trie struct

--- src/data.rs
+++ src/data.rs
@@ -1,40 +1,45 @@
-struct Node { children: Vec<Option<Box<Node>>>, 
+use std::collections::HashMap;
+
+#[derive(Clone)]
+struct Node {
+    children: HashMap<char, Node>,
     is_end_of_word: bool,
 }
-impl Node { fn new() -> Node { Node { children: vec![None; 
-            26], // 26 letters in English alphabet 
+impl Node {
+    fn new() -> Node {
+        Node {
+            children: HashMap::new(), // 26 letters in English alphabet
             is_end_of_word: false,
         }
     }
 }
-struct Trie { root: Node,
+pub struct Trie {
+    root: Node,
 }
-impl Trie { fn new() -> Trie { Trie { root: Node::new() }
+impl Trie {
+    pub fn new() -> Trie {
+        Trie { root: Node::new() }
     }
-    
-    fn insert(&mut self, word: &str) { let mut current_node 
-        = &mut self.root; for c in word.chars() {
-            let index = (c as u8 - b'a') as usize; // 
-            convert char to index if 
-            current_node.children[index].is_none() {
-                current_node.children[index] = 
-                Some(Box::new(Node::new()));
-            }
-            current_node = 
-            current_node.children[index].as_mut().unwrap();
+
+    pub fn insert(&mut self, word: &str) {
+        let mut current_node = &mut self.root;
+        for c in word.chars() {
+            current_node = current_node
+                .children
+                .entry(c)
+                .or_insert_with(|| Node::new());
         }
         current_node.is_end_of_word = true;
     }
-    
-    fn search(&self, word: &str) -> bool { let mut 
-        current_node = &self.root; for c in word.chars() {
-            let index = (c as u8 - b'a') as usize; // 
-            convert char to index if 
-            current_node.children[index].is_none() {
+
+    pub fn search(&self, word: &str) -> bool {
+        let mut current_node = &self.root;
+        for c in word.chars() {
+            if let Some(child) = current_node.children.get(&c) {
+                current_node = child;
+            } else {
                 return false;
             }
-            current_node = 
-            current_node.children[index].as_ref().unwrap();
         }
         current_node.is_end_of_word
     }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
+#![feature(test)]
+
+extern crate test;
+
+use data::Trie;
 use itertools::Itertools;
 use once_cell::sync::Lazy;
 use std::io;
 
 mod data;
 
-static DICT: Lazy<Vec<String>> = Lazy::new(|| {
-    include_str!("german.dic")
+static DICT: Lazy<Trie> = Lazy::new(|| {
+    let words: Vec<String> = include_str!("german.dic")
         .to_uppercase()
         .split('\n')
         .filter_map(|str| {
@@ -16,8 +21,21 @@ static DICT: Lazy<Vec<String>> = Lazy::new(|| {
                 Some(x)
             }
         })
-        .collect()
+        .collect();
+
+    let mut trie = Trie::new();
+    for word in words {
+        trie.insert(&word);
+    }
+
+    trie
 });
+
+struct Possibilities {
+    full_matches: Vec<String>,
+    all_combinations: Vec<String>,
+    compound_matches: Vec<String>,
+}
 
 fn main() -> io::Result<()> {
     let stdin = io::stdin();
@@ -30,65 +48,105 @@ fn main() -> io::Result<()> {
             break;
         }
 
-        let word_pieces: Vec<&str> = buffer.split(' ').collect();
-        let len = word_pieces.len();
-        let permutations: Vec<Vec<&str>> = word_pieces.into_iter().permutations(len).collect();
-        let mut full_matches: Vec<String> = Vec::new();
-        let mut all_combinations: Vec<String> = Vec::new();
-        let mut compound_matches: Vec<String> = Vec::new();
+        let possibilities = get_possibilities(&buffer);
 
-        for possibility in permutations {
-            let mut full = String::new();
-            for piece in possibility {
-                full += piece;
+        if !possibilities.full_matches.is_empty() {
+            println!("Volltreffer:");
+            for full_match in possibilities.full_matches {
+                println!("{full_match}");
             }
-            let full = full.to_uppercase();
-            if DICT.contains(&full) {
-                full_matches.push(full);
+        } else if !possibilities.compound_matches.is_empty() {
+            println!("Folgende Möglichkeiten gefunden:");
+            for compound_match in possibilities.compound_matches {
+                println!("{compound_match}");
             }
-            else if check_match(&full) {
-                compound_matches.push(full);
-            } else {
-                all_combinations.push(full);
+        } else if !possibilities.all_combinations.is_empty() {
+            println!("Keine Übereinstimmung gefunden. Zeige alle Kombinationen:");
+            for combi in possibilities.all_combinations {
+                println!("{combi}");
             }
+        } else {
+            println!("Nichts gefunden!");
         }
-
-            if !full_matches.is_empty() {
-                println!("Volltreffer:");
-                for full_match in full_matches {
-                    println!("{full_match}");
-                }
-            }
-            else if !compound_matches.is_empty() {
-                println!("Folgende Möglichkeiten gefunden:");
-                for compound_match in compound_matches {
-                    println!("{compound_match}");
-                }
-            } else if !all_combinations.is_empty() {
-                println!("Keine Übereinstimmung gefunden. Zeige alle Kombinationen:");
-                for combi in all_combinations {
-                    println!("{combi}");
-                }
-            } else {
-                println!("Nichts gefunden!");
-            }
         println!();
     }
     Ok(())
 }
 
+fn get_possibilities(s: &str) -> Possibilities {
+    let word_pieces: Vec<&str> = s.split(' ').collect();
+    let len = word_pieces.len();
+    let permutations: Vec<Vec<&str>> = word_pieces.into_iter().permutations(len).collect();
+    let mut full_matches: Vec<String> = Vec::new();
+    let mut all_combinations: Vec<String> = Vec::new();
+    let mut compound_matches: Vec<String> = Vec::new();
+
+    for possibility in permutations {
+        let mut full = String::new();
+        for piece in possibility {
+            full += piece;
+        }
+        let full = full.to_uppercase();
+        if DICT.search(&full) {
+            full_matches.push(full);
+        } else if check_match(&full) {
+            compound_matches.push(full);
+        } else {
+            all_combinations.push(full);
+        }
+    }
+
+    full_matches.dedup();
+    all_combinations.dedup();
+    compound_matches.dedup();
+
+    Possibilities {
+        full_matches,
+        all_combinations,
+        compound_matches,
+    }
+}
+
 fn check_match(input: &str) -> bool {
-    for ele in DICT.iter() {
-        if input.starts_with(ele) {
-            let reduce: String = input.chars().skip(ele.chars().count()).collect();
-            if reduce.is_empty() {
+    let mut chars = input.chars().peekable();
+    let mut buffer = String::new();
+
+    while let Some(c) = chars.next() {
+        buffer.push(c);
+
+        let result = DICT.search(&buffer);
+        if !result {
+            continue;
+        }
+        if chars.peek().is_some() {
+            let s: String = chars.clone().collect();
+            if DICT.search(&s) {
                 return true;
             }
-            if check_match(&reduce) {
-                return true;
-            }
+        } else {
+            return true;
         }
     }
 
     false
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test::Bencher;
+
+    #[bench]
+    fn bench_xor_1000_ints(b: &mut Bencher) {
+        let cases = vec!["kaf sse ta fee", "hte n ac rb ah", "efg ge ara ti"];
+
+        b.iter(|| {
+            // Use `test::black_box` to prevent compiler optimizations from disregarding
+            // Unused values
+
+            for ele in &cases {
+                get_possibilities(ele);
+            }
+        });
+    }
 }
